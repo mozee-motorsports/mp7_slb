caedab94ae1eb6b942896477a2cb5d75da78b0bc
Zach Gardipee <gardipeez@msoe.edu>
2024-11-11 11:37:19 -0600

    updated stff

--- ShutdownLoop/Core/Src/main.c
+++ ShutdownLoop/Core/Src/main.c
@@ -94,10 +94,10 @@ int main(void)
   /* USER CODE END 2 */
 
   /* Initialize led */
-  //BSP_LED_Init(LED_GREEN);
+  BSP_LED_Init(LED_GREEN);
 
   /* Initialize USER push-button, will be used to trigger an interrupt each time it's pressed.*/
-  //BSP_PB_Init(BUTTON_USER, BUTTON_MODE_EXTI);
+  BSP_PB_Init(BUTTON_USER, BUTTON_MODE_EXTI);
 
   /* Initialize COM1 port (115200, 8 bits (7-bit data + 1 stop bit), no parity */
   BspCOMInit.BaudRate   = 115200;
