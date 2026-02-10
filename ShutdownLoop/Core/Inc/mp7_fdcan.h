ce3c806618ecd0b3375199905c216a61fbb0d562
Jacob Gundlach <gundlachj@msoe.edu>
2026-02-09 20:48:20 -0600

    added CAN IDs from MP7 Standard

--- ShutdownLoop/Core/Inc/mp7_fdcan.h
+++ ShutdownLoop/Core/Inc/mp7_fdcan.h
@@ -23,6 +23,43 @@
 #error "HAL Library Files Not Found"
 #endif
 
+/*************************/
+// CAN Messages
+/*************************/
+/*
+ * CAN DLCs
+ */
+#define MP7_CAN_MAX_DLC             8
+
+/*
+ * CAN IDs
+ */
+// High Priority Range
+#define MP7_HIGH_PRIORITY_MIN_ID  0x000
+#define MP7_HIGH_PRIORITY_MAX_ID  0x3FF
+
+// Error ID Range
+#define MP7_ERROR_MIN_ID  0x000
+#define MP7_ERROR_MAX_ID  0x1E5
+
+// Normal ID Range
+#define MP7_NORMAL_MIN_ID 0x1E6
+#define MP7_NORMAL_MAX_ID 0x3CC
+
+// Inverter IDs (offset of 0x3DD)
+#define INVERTER_ID_MIN_ID  0x3DD
+#define INVERTER_ID_MAX_ID  0x3FF
+
+// Battery Management System (BMS) IDs
+#define MP7_BMS_MIN_ID  0x3CD
+#define MP7_BMS_MAX_ID  0x3DC
+
+// Low Priority Range
+#define MP7_LOW_PRIORITY_MIN_ID 0x400
+#define MP7_LOW_PRIORITY_MAX_ID 0x7FF
+
+
+
 HAL_StatusTypeDef MP7_FDCAN_ConfigureGlobalFilter(FDCAN_HandleTypeDef *hfdcan);
 
 HAL_StatusTypeDef MP7_FDCAN_ConfigureFilter(FDCAN_HandleTypeDef *hfdcan,
